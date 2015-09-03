205a786cd7e0aa1d32b1752c98f16a69e0876515
yutaka-sano <yutaka.sano@dena.com>
2015-09-03 13:19:48 +0900

    Initial Commit

---
+++ Swift-Twitter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift-Twitter
+//
+//  Created by Yutaka Sano on 2015/09/03.
+//  Copyright (c) 2015年 Yutaka Sano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
